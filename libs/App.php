895ad36d190c426b2ae78a785f86c685a4088c2d
Rafiqul Islam <rafiquljakir@gmail.com>
2024-03-23 16:45:40 +0600

    Creating Registration and login Methods

--- libs/App.php
+++ libs/App.php
@@ -85,12 +85,42 @@
 
             $delete_record = $this->link->query($query);
             $delete_record->execute();
-            
+
             header("location: ".$path."");
             
         }
 
+        
+        //register Operation
+        public function register($query, $arr, $path){
+            if($this->validate($arr) == "empty"){
+                echo "<script>alert('one or more input fields are empty !!') </script>";
+            }else{
+                $register_user = $this->link->prepare($query);
+                $register_user->execute($arr);
+                
+                header("location: ".$path."");
+            }
+        }
 
+        //login operation
+        public function login($query, $data, $path){
+            
+            //validate email;
+
+            $login_user = $this->link->query($query);
+            $login_user->execute();
+            $fetch = $login_user->fetch(PDO::FETCH_OBJ);
+            if($login_user->rowCount() > 0){
+                if(password_verify($data['password'],$fetch['password'])){
+                    //start session
+
+
+                    header("location:".$path."");
+
+                }
+            }
+        }
 
 
 
